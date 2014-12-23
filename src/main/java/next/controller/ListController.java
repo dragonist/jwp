e22082aa4353ce618620a72ee34c154ce7af6c5d
이경륜 <erin314@naver.com>
2014-12-23 16:21:27 +0900

    #006 멀티 쓰레드 완성

--- src/main/java/next/controller/ListController.java
+++ src/main/java/next/controller/ListController.java
@@ -12,15 +12,15 @@ import core.mvc.ModelAndView;
 
 public class ListController extends AbstractController {
 	private QuestionDao questionDao = new QuestionDao();
-	private List<Question> questions;
 	
 	@Override
 	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		questions = questionDao.findAll();
+		List<Question> questions = questionDao.findAll();
 		
 		ModelAndView mav = jstlView("list.jsp");
 		mav.addObject("questions", questions);
 		return mav;
+		
 	}
 }
